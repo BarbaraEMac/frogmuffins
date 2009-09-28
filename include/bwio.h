d06bd42d63f0b0478abf6caeed3af24deac8b0f8
DanielGoc <DanielGoc@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-09-28 23:00:01 +0000

    adding the rest of files?

---
+++ include/bwio.h
@@ -0,0 +1,53 @@
+/*
+ * bwio.h
+ */
+
+typedef char *va_list;
+
+#define __va_argsiz(t)	\
+		(((sizeof(t) + sizeof(int) - 1) / sizeof(int)) * sizeof(int))
+
+#define va_start(ap, pN) ((ap) = ((va_list) __builtin_next_arg(pN)))
+
+#define va_end(ap)	((void)0)
+
+#define va_arg(ap, t)	\
+		 (((ap) = (ap) + __va_argsiz(t)), *((t*) (void*) ((ap) - __va_argsiz(t))))
+
+#define COM1	0
+#define COM2	1
+
+#define ON	1
+#define	OFF	0
+
+int* clock_init( int clock_base, int enable, int val );
+
+void clock_stop( int clock_base );
+
+void wait( int ms );
+
+int bwsetfifo( int channel, int state );
+
+int bwsetspeed( int channel, int speed );
+
+int bwsendc( int channel, char c, int timeout );
+
+int bwputc( int channel, char c );
+
+int bwgetc( int channel );
+
+int bwreadc( int channel, char *c, int timeout );
+
+void bwclear( int channel );
+
+int bwputx( int channel, char c );
+
+int bwputstr( int channel, char *str );
+
+int bwputr( int channel, unsigned int reg );
+
+void bwputw( int channel, int n, char fc, char *bf );
+
+void bwprintf( int channel, char *format, ... );
+
+int sscanf( char *src, char *format, ... );
