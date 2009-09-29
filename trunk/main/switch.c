416325ef396a44fa5968fff4a524d498a6cd3de4
DanielGoc <DanielGoc@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-09-29 19:51:24 +0000

    added generic syscall function

--- trunk/main/switch.c
+++ trunk/main/switch.c
@@ -1,9 +1,11 @@
 #include "switch.h"
 // This is a file used to made switch.c
 // it should not be used
-void kerEnt() {
-
+void kernEnter() {
 }
 
-void kerExit(struct taskdesc *active, struct request *req) {
+void kernExit(struct taskdesc *active, struct request *req) {
+}
+
+int syscall(int arg0, int arg1, int arg3, int type) {
 }
