f4cd1e0e4ac8fd0adda82328434f3ae088031c55
DanielGoc <DanielGoc@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-09-29 19:56:34 +0000

    renamed kernel Entry and Exit

--- main/switch.c
+++ main/switch.c
@@ -1,11 +1,12 @@
 #include "switch.h"
 // This is a file used to made switch.c
 // it should not be used
-void kernEnter() {
+void kernelEnter() {
 }
 
-void kernExit(struct taskdesc *active, struct request *req) {
+void kernelExit(struct taskdesc *active, struct request *req) {
 }
 
 int syscall(int arg0, int arg1, int arg3, int type) {
+	return 0;
 }
