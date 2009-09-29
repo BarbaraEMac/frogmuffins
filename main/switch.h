9c47084f4f3029cc20e463e3f61c9c836f5e1a97
z4beth <z4beth@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-09-29 15:02:13 +0000

    Added comments to the TD members. Added PQ links to TDs

--- main/switch.h
+++ main/switch.h
@@ -1,17 +1,20 @@
+/**
+ * CS 452: Kernel
+ * becmacdo
+ * dgoc
+ */
+
 enum TASK_STATE {
-    ACTIVE = 0,
-    READY,
-    BLOCKED,
-    DEFUNCT
+    ACTIVE = 0,         // Only 1 task will ever be active
+    READY,              // Task may be selected to be active
+    BLOCKED,            // Task is waiting for something
+    DEFUNCT             // Task will never run again :(
 } taskState;
 
 typedef struct taskdes {
-	// State
-    int spsr;
-    // Stack Pointer
-	int sp;
-    // The first function this runs
-	void (* start )();
+    int spsr;           // Saved Processor State Register
+	int sp;             // Stack Pointer
+	void (* start )();  // The first function this runs
 
     int id;             // A unique identifying id
     int parentId;       // The unique id of the parent
@@ -19,11 +22,11 @@ typedef struct taskdes {
     int returnValue;    // What the heck is this?
 
     int priority;       // A priority value (ranges from 0->2)
-    
-    enum TASK_STATE state;
+                        
+    enum TASK_STATE state; // State of the task
 
-    //TD *nextPQ;
-    //TD *prevPQ;
+    struct taskdes *nextPQ; // Link to the next TD in the PQ
+    struct taskdes *prevPQ; // Link to the prev TD in the PQ
 
 } TD;
 
