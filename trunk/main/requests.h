98fff5bb782b84b61930f6ea5343205f00527ef1
DanielGoc <DanielGoc@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-09-30 19:55:47 +0000

    fized 5 paramenter passing

--- trunk/main/requests.h
+++ trunk/main/requests.h
@@ -19,11 +19,16 @@ enum RequestCode {
 };
 
 typedef struct {
+	enum RequestCode type; // the type of the request (including number of arguments)
+	int const * const args;	// place in user memory where arguments are stored
+	// NOTE: The 5th argument is in args[22] 
+	// and NOT args[4] as would be expected
     int arg0;   // First argument to function
 	int arg1;   // Second argument to function
 	int arg2;   // Third argument to function
 	int arg3;   // Fourth argument to function
-	enum RequestCode type; //   
+	
+
 } Request;
 
 /**
@@ -50,5 +55,7 @@ void Pass ();
  * Terminate execution forever.
  */
 void Exit ();
+
+int Test5( int a0, int a1, int a2, int a3, int a5) ;
 
 #endif
