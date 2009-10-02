464739d175d1197ab59a66998727ac7c1ff47009
DanielGoc <DanielGoc@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-10-02 21:24:52 +0000

    changed MAX_PRIORITIES to NUM_PRIORITIES for more readability (there was a related bug as well)

--- main/assert.h
+++ main/assert.h
@@ -12,9 +12,13 @@ void __assert ( int test, char *exp, int line, char *file );
 
 #define assert(exp) __assert(exp, #exp, __LINE__, __FILE__ )
 
+#define debug(args...) bwprintf( COM2, args )
+
 #else
 
 #define assert(exp) do {} while (0)
+
+#define debug(args...) do {} while(0)
 
 #endif
 
