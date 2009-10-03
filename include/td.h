61fe73f2525aac4ed3f20a876802042a0c6fd2b1
z4beth <z4beth@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-10-03 23:21:22 +0000

    Cleaning up comments.

--- include/td.h
+++ include/td.h
@@ -3,10 +3,11 @@
  * becmacdo
  * dgoc
  */
+
 #ifndef __TD_H__
 #define __TD_H__
 
-#define NUM_PRIORITY 	3
+#define NUM_PRIORITY 	3	// For now, we will use 3 priorities [0,2]
 #define USER_START 		0x00044f88
 #define USER_END		0x01fdd000
 
@@ -14,61 +15,109 @@
 typedef void (* Task) ();
 
 enum TASK_STATE {
-	ACTIVE = 0,		 // Only 1 task will ever be active
+	ACTIVE = 0,		 	// Only 1 task will ever be active
 	READY,				// Task may be selected to be active
 	BLOCKED,			// Task is waiting for something
-	DEFUNCT			 // Task will never run again :(
+	DEFUNCT			 	// Task will never run again :(
 } taskState;
 
+// We want to call task descriptors TDs for short.
 typedef struct taskdesc TD;
+
+// At some points, a TD* is a queue.
 typedef TD* Queue;
 
+/**
+ * A Task Descriptor that the kernel uses to define a user task.
+ */
 struct taskdesc {
 	int spsr;			// Saved Processor State Register
 	int *sp;			// Stack Pointer
-	int returnValue;	// Value to pass to asm if we need to return anything to a syscall
-	
+	int returnValue;	// Value to pass to asm if we need to 
+						// return anything to a syscall
 	int id;			 	// A unique identifying id
 	int parentId;		// The unique id of the parent
 
-	// TODO: Do not store the priority of the task?
-	// Yes, because the task might ask for it.
 	int priority;		// A priority value (ranges from 0->4)
 						
-	enum TASK_STATE state;	// State of the task
+	enum TASK_STATE state;	// State of the task - see enum above
 
-	TD *nextPQ; // Link to the next TD in the PQ
-	TD *prevPQ; // Link to the prev TD in the PQ
+	TD *nextPQ; 		// Link to the next TD in the PQ
+	TD *prevPQ; 		// Link to the prev TD in the PQ
 };
 
+/**
+ * The task descriptor priority queues.
+ */
 typedef struct {
 	
-	TD tdArray[64];
+	TD tdArray[64];		// Use this until we have dynamic memory management
 
-	int frontPtr;	// Not currently used
-	int backPtr;	
+	int frontPtr;		// TODO: Not currently used
+	int backPtr;		// Points to the next unused TD in the array
+			
+    Queue ready[NUM_PRIORITY]; 	// The ready queue
+	int highestPriority;		// The highest non-empty bucket in the ready Q
 
-    Queue ready[NUM_PRIORITY];
-	int highestPriority;
+	int nextId;			// TODO: Should be the same as backPtr (off by 1)
 
-	int nextId;		// TODO: Should be the same as backPtr (off by 1)
+	Queue blocked;		// A single queue of blocked tasks
 
-	Queue blocked;
-
-	Queue defunct;	// Not currently used
+	Queue defunct;		// TODO: Not currently used
 
 } PQ;
 
-//TODO: Comments
+
+/**
+ * Initialize the priority queues and TDs in the td array.
+ * pq - The PQ to init.
+ */
 void pq_init (PQ *pq);
-TD * td_init (int priority, Task start, int parentId, PQ *pq);
 
-void pq_insert (PQ *pq, TD *td);
-TD *pq_popReady (PQ *pq);
-
-void queue_push (Queue *head, TD *newTail);		// Returns the new head of the Queue
-TD * queue_pop  (Queue *head);
-
+/**
+ * Create a new TD.
+ * priority - The priority for the new TD.
+ * start - The user task function.
+ * parentId - The id of the parent who created this task.
+ * pq - The priority queue manager.
+ * RETURN: A pointer to the new TD.
+ */
 TD * td_create (int priority, Task start, int parentId, PQ *pq);
 
+/**
+ * Initialize a single td.
+ * priority - The priority for the new TD.
+ * start - The user task function.
+ * parentId - The id of the parent who created this task.
+ * pq - The priority queue manager.
+ * RETURN: A pointer to the new TD.
+ */
+TD * td_init (int priority, Task start, int parentId, PQ *pq);
+
+/**
+ * Inserts the TD into an appropriate priority queue.
+ * pq - The priority queue manager.
+ * td - The task descriptor to insert.
+ */
+void pq_insert (PQ *pq, TD *td);
+
+/**
+ * Pops the highest priority TD off of the ready queue.
+ * pq - The priority queue manager.
+ * RETURN: The highest priority READY TD.
+ */
+TD *pq_popReady (PQ *pq);
+
+/**
+ * Given a pointer to the head of a queue, push a new entry as the tail.
+ * head - A pointer to the head of the queue.
+ * newTail - The TD to insert onto the queue.
+ */
+void queue_push (Queue *head, TD *newTail);
+
+/**
+ * Given a pointer to the head of a queue, pop the head off it.
+ * RETURN: The newly popped head TD.
+ */
+TD * queue_pop  (Queue *head);
 #endif
