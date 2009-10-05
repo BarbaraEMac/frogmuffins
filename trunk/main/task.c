50781d7c45e11e443fa11fc55ff6039138cf8359
z4beth <z4beth@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-10-05 15:03:56 +0000

    This is where user task stuff goes. (ie. Not kernel code.

---
+++ trunk/main/task.c
@@ -0,0 +1,17 @@
+/*
+ * CS 452: Kernel
+ * becmacdo
+ * dgoc
+ *
+ * User task stuff goes in here.
+ */
+
+#define DEBUG
+#include <bwio.h>
+#include <ts7200.h>
+
+#include "debug.h"
+#include "requests.h"
+#include "task.h"
+
+
