f0c54ac85d89847b4e77e515d9fd9f014cf4533f
z4beth <z4beth@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-10-05 14:35:51 +0000

    Changed assert.h reference to debug.h

--- src/debug.c
+++ src/debug.c
@@ -4,21 +4,15 @@
  * becmacdo
  * dgoc
  */
-
-#include "assert.h"
-
 #include <bwio.h>
 #include <ts7200.h>
+
+#include <debug.h>
 
 void __assert ( int test, char *exp, int line, char *file ) {
 	
 	if ( !test ) {
 		bwprintf (COM2, "Assert Failure: %s at line #%d in file %s.\n\r", exp, line, file );
-		// TODO: Stack trace
+		// TODO: Stack trace would be nice ...
 	}
 }
-
-
-
-
-
