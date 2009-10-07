a5999139ceba5f20322aeebd78bf7f73106520d6
z4beth <z4beth@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-10-07 15:33:55 +0000

    Added some new error codes.

--- trunk/include/error.h
+++ trunk/include/error.h
@@ -19,8 +19,14 @@ enum ERROR {
 	PQ_FULL = -8,
 	NULL_ADDR = -9,
 	SNDR_NOT_RPLY_BLKD = -10,
+	BUFFER_FULL = -11,
+	INVALID_PRIORITY = -12,
+	NO_TDS_LEFT = -13,
+	INVALID_START_FCN = -14,
+	
 
 } error;
 
+#define printErrorType (e) { bwprintf (COM2, "\r\nError: %s.", #e); }
 
 #endif
