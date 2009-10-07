9c050f4e3416469276c855504de12e0d08f4bb92
z4beth <z4beth@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-10-07 21:17:38 +0000

    Adding a Rock, Paper, Scissors game server.

---
+++ task/gameserver.c
@@ -0,0 +1,74 @@
+/*
+ * CS 452: Kernel
+ * becmacdo
+ * dgoc
+ *
+ * A Rock, Paper, Scissors Server
+ */
+
+#define DEBUG
+#define	MSG_LEN		1024
+
+#include <bwio.h>
+#include <ts7200.h>
+
+#include "debug.h"
+#include "globals.h"
+#include "requests.h"
+#include "gameserver.h"
+
+void rps_run () {
+
+	int senderTid;
+	char msgBuffer[MSG_LEN];
+	PlayerRequest req;
+	Player tmpPlayer;
+	RPSserver server;
+	
+	
+	// Initialize the Rock, Paper, Scissors Server
+	rps_init (&server);
+
+	FOREVER {
+		Receive ( &senderTid, msgBuffer, MSG_LEN );
+
+		memcpy((char*)&req, msgBuffer, sizeof(PlayerRequest));
+
+		switch(req.type) {
+			case SIGNUP:
+				// Add to the players queue
+				// If 2 players, start them playing by replying to each
+				// Otherwise, do nothing
+
+				break;
+			
+			case PLAY:
+				// Store the move
+				// If opponent has played, determine the winner
+					// Reply to both
+				// Else,
+					// Wait for opponent to make their move (do nthing)
+				break;
+			
+			
+			
+			case QUIT:
+				// Stop accepting calls from this player
+				// On the next PLAY call from their opponent, send that this
+				// one quit.
+				break;
+		
+			
+			default:
+				error (1, "Invalid RPS type.");
+				break;
+		}
+	}
+
+}
+
+void rps_init (RPSserver *server) {
+	server->players = 0;
+}
+
+
