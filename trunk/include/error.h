7ddd69a1c6f8ef933a4b0e5ab47a2a8c8ffae0cc
z4beth <z4beth@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-10-07 16:48:06 +0000

    Fixed numbers

--- trunk/include/error.h
+++ trunk/include/error.h
@@ -9,20 +9,19 @@
 
 enum ERROR {
 	NO_ERROR = 0,
-	NEG_TID = -1,
-	NUM_TID = -2,
-	DEFUNCT_TID = -3,
-	OLD_TD = -4, 		// Not from the current generation
-	OUT_OF_BOUNDS = -5,
-	RCV_BUFFER_FULL = -6,
-	RPLY_BUFFER_FULL = -7,
-	PQ_FULL = -8,
-	NULL_ADDR = -9,
-	SNDR_NOT_RPLY_BLKD = -10,
-	BUFFER_FULL = -11,
-	INVALID_PRIORITY = -12,
-	NO_TDS_LEFT = -13,
-	INVALID_START_FCN = -14,
+	NEG_TID = -10000,
+	DEFUNCT_TID,
+	OLD_TD, 		// Not from the current generation
+	OUT_OF_BOUNDS,
+	RCV_BUFFER_FULL,
+	RPLY_BUFFER_FULL,
+	PQ_FULL,
+	NULL_ADDR,
+	SNDR_NOT_RPLY_BLKD,
+	BUFFER_FULL,
+	INVALID_PRIORITY,
+	NO_TDS_LEFT,
+	INVALID_START_FCN,
 	
 
 } error;
