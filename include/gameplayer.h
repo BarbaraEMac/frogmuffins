cb52ca4fb525734e28a7fcf79cfb66ea13b65897
z4beth <z4beth@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-10-08 15:42:11 +0000

    Adding a game player who talks to the game server.

---
+++ include/gameplayer.h
@@ -0,0 +1,16 @@
+/*
+ * CS 452: Kernel
+ * becmacdo
+ * dgoc
+ *
+ * A Rock, Paper, Scissors Player
+ */
+
+void genericPlayer (char *name, char move, int timesToPlay);
+
+void rockPlayer();
+
+void paperPlayer();
+
+void scissorsPlayer();
+
