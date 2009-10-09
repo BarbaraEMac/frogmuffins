23a395ffe57903be9357abfd5b271dad965c610e
z4beth <z4beth@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-10-09 00:19:18 +0000

    Added many debug statements and fixed some issues

--- server/nameserver.c
+++ server/nameserver.c
@@ -3,6 +3,7 @@
  * becmacdo
  * dgoc
  */
+//#define DEBUG
 
 #include <bwio.h>
 
@@ -12,7 +13,7 @@
 
 
 void ns_run () {
-
+	debug ("ns_run: The Name Server is about to start. \r\n");	
 	NameServer ns;
 	int 	   senderTid;
 	NSRequest  req;
@@ -23,12 +24,16 @@ void ns_run () {
 
 	FOREVER {
 		// Receive a server request
+		debug ("Name Server is about to receive. \r\n");
 		Receive ( &senderTid, (char *) &req, sizeof(NSRequest) );
+		debug ("ns Receive: fromTid=%d name=%s type=%d \r\n", senderTid, req.name, req.type);
 
 		switch (req.type) {
 			case REGISTERAS:
 				// TODO return error if the name already exists
 				ns_insert (&ns, req.name, senderTid);
+
+				assert ( ns_lookup(&ns, req.name) == senderTid );
 				break;
 			case WHOIS:
 				ret = ns_lookup (&ns, req.name);
@@ -40,16 +45,23 @@ void ns_run () {
 				break;
 		}
 
+		debug ("Name Server is about to reply to %d. \r\n", senderTid);
 		Reply ( senderTid, (char *) &ret, sizeof(int) );
+		debug ("Name Server returned from Replying to %d. \r\n", senderTid);
+
+		// TODO: Clear out req? Is this needed?
+		req.name[0] = 0;
+		req.type = 0;
 	}
 }
 
 void ns_init (NameServer *ns) {
+	debug ("ns_init: name server=%x \r\n", ns);
 	ns->ptr = 0;
 }
 
 void ns_insert (NameServer *ns, const char *name, int tid) {
-	assert ( itr < NUM_NS_ENTRIES );
+	debug ("ns_insert: ns=%x name=%s tid=%d ptr=%d\r\n", ns, name, tid, ns->ptr);
 	
 	int itr = 0;
 	NSEntry e;
@@ -58,26 +70,33 @@ void ns_insert (NameServer *ns, const char *name, int tid) {
 	
 	// Determine if this name is already in the server.
  	NSEntry *tmp;
-	do {
+	while ( itr < ns->ptr ) {	
 		tmp = &ns->entries[itr];
 		if ( strcmp ( tmp->name, name ) == 0 ) {
+			debug ("   ns_insert: Resetting tid of %s to %d \r\n", name, tid);
 			tmp->tid = tid;
 			break;
 		}
-	} while ( ++itr != ns->ptr );
-
+		itr ++;
+	}
 	// Add the entry if the name isn't in the server.
-	if (itr == ns->ptr) {
+	if (itr >= ns->ptr) {
+		debug ("   ns_insert: Adding %s, since it is not in. \r\n", name);
 		ns->entries[ns->ptr++] = e; 
 	}
+	assert ( itr < NUM_NS_ENTRIES );
 }
 
 int ns_lookup (NameServer *ns, const char *name) {
+	debug ("ns_lookup: ns=%x name=%s \r\n", ns, name);
 	int itr = ns->ptr;
 	
 	assert ( itr < NUM_NS_ENTRIES );
 	// Linearly traverse through the entries
-	while ( --itr > 0 ) {
+	while ( --itr >= 0 ) {
+		debug ("   %d->{%s, %d} == %s ? %d\r\n", itr, 
+			   ns->entries[itr].name, ns->entries[itr].tid,
+			   name, strcmp(name, ns->entries[itr].name));
 		// If we've found a match,
 		if ( strcmp (ns->entries[itr].name, name) == 0 ) {
 			// Return the corresponding tid
@@ -86,5 +105,6 @@ int ns_lookup (NameServer *ns, const char *name) {
 	}
 	
 	// Otherwise, return an error
+	debug("	  ns_lookup: %s NOT_FOUND \r\n", name);
 	return NOT_FOUND;
 }
