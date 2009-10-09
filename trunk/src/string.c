571e07ec9b9a01bdfb9fd3bbc8415b2e8d9b946a
z4beth <z4beth@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-10-09 00:40:12 +0000

    Fixing that darn strcmp bug still

--- trunk/src/string.c
+++ trunk/src/string.c
@@ -24,11 +24,17 @@ char * memcpy ( char * destination, const char * source, size_t num ) {
 }
 
 int strcmp (const char *str1, const char *str2) {
-	while ( *str1 == *str2 && *(str1++) != 0 && *(str2++) != 0 ) {}
-	
-	if ( *str1 == 0 && *str2 == 0 ) {
-		return 0;
-	}
+	while ( *str1 == *str2 ) {
+		str1 ++;
+		str2 ++;
 
+		if ( (*str1 == 0) && (*str2 == 0) ) {
+			return 0;
+		}
+		if ( (*str1 == 0) || (*str2 == 0) ) {
+			break;
+		}
+	}
+	
 	return (*str1 < *str2) ? -1 : 1;
 }
