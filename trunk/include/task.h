7400e93018f4fafa3a308fdc2614e7274a0a748d
z4beth <z4beth@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-10-09 00:20:19 +0000

    Restructured the file to include all testing user tasks from K1 and K2

--- trunk/include/task.h
+++ trunk/include/task.h
@@ -9,12 +9,29 @@
 #ifndef __TASK_H__
 #define __TASK_H__
 
+//-------------------------------------------------------------------------
+//---------------------------Kernel 2--------------------------------------
+//-------------------------------------------------------------------------
 void k2_firstUserTask();
+
+/**
+ * The following 2 functions test the name server.
+ * They spawn many tasks that RegisterAs and verify WhoIs is correct.
+ */
+void k2_registerTask1();
+void k2_registerTask2();
+
+/**
+ * The following 2 functions verify that basic Send and Receive work.
+ */
 void k2_senderTask();
 void k2_receiverTask();
 
+
+//-------------------------------------------------------------------------
+//---------------------------Kernel 1--------------------------------------
+//-------------------------------------------------------------------------
 void k1_firstUserTask();
 void k1_userTaskStart();
-
 
 #endif
