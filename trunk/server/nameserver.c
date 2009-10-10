1ebd8bcf16dcc2f4f81ef41d113ff8e8b0e8fbfe
DanielGoc <DanielGoc@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-10-10 20:58:33 +0000

    cleaned up the code, added asserts, fixed a minor bug in ns_lookup that started looking one past the last saved name

--- trunk/server/nameserver.c
+++ trunk/server/nameserver.c
@@ -3,7 +3,7 @@
  * becmacdo
  * dgoc
  */
-#define DEBUG
+//#define DEBUG
 
 #include <bwio.h>
 
@@ -17,94 +17,89 @@ void ns_run () {
 	NameServer ns;
 	int 	   senderTid;
 	NSRequest  req;
-	int 	   ret;
+	int 	   ret, len;
 
 	// Initialize the Name Server
 	ns_init (&ns);
 
 	FOREVER {
 		// Receive a server request
-		debug ("Name Server is about to receive. \r\n");
-		Receive ( &senderTid, (char *) &req, sizeof(NSRequest) );
-		debug ("ns Receive: fromTid=%d name=%s type=%d \r\n", senderTid, req.name, req.type);
+		debug ("ns: is about to Receive. \r\n");
+		len = Receive ( &senderTid, (char *) &req, sizeof(NSRequest) );
+		debug ("ns: Received: fromTid=%d name='%s' type=%d  len=%d\r\n", senderTid, req.name, req.type, len);
+	
+		assert( len == sizeof(NSRequest) );
 
 		switch (req.type) {
 			case REGISTERAS:
 				// TODO return error if the name already exists
 				ns_insert (&ns, req.name, senderTid);
 
-				assert ( ns_lookup(&ns, req.name) == senderTid );
+				assert ( ns_lookup(&ns, req.name)->tid == senderTid );
 				break;
 			case WHOIS:
-				ret = ns_lookup (&ns, req.name);
+				ret = ns_lookup(&ns, req.name)->tid;
 				break;
 			default:
-				ret = NS_INVALID_REQ;
+				ret = NS_INVALID_REQ_TYPE;
 				// TODO: do we need this message?
 				error (1, "Nameserver request type is not valid.");
 				break;
 		}
 
-		debug ("Name Server is about to reply to %d. \r\n", senderTid);
+		debug ("ns: about to Reply to %d. \r\n", senderTid);
 		Reply ( senderTid, (char *) &ret, sizeof(int) );
-		debug ("Name Server returned from Replying to %d. \r\n", senderTid);
+		debug ("ns: returned from Replying to %d. \r\n", senderTid);
 
-		// TODO: Clear out req? Is this needed?
-		req.name[0] = 0;
-		req.type = 0;
+		// No need to clear out req, it's completely overwritten by Receive
 	}
 }
 
 void ns_init (NameServer *ns) {
 	debug ("ns_init: name server=%x \r\n", ns);
-	ns->ptr = 0;
+	ns->nextEmpty = 0;
 }
 
 void ns_insert (NameServer *ns, const char *name, int tid) {
-	debug ("ns_insert: ns=%x name=%s tid=%d ptr=%d\r\n", ns, name, tid, ns->ptr);
+	debug ("ns_insert: ns=%x name='%s' tid=%d nextEmpty=%d\r\n", ns, name, tid, ns->nextEmpty);
 	
-	int itr = 0;
-	NSEntry e;
-	strncpy (e.name, name, NAME_LEN); // Deep copy the string so we have it
-	e.tid = tid;
+	NSEntry *e;
+	assert ( ns->nextEmpty < NUM_NS_ENTRIES );
 	
 	// Determine if this name is already in the server.
- 	NSEntry *tmp;
-	while ( itr < ns->ptr ) {	
-		tmp = &ns->entries[itr];
-		if ( strcmp ( tmp->name, name ) == 0 ) {
-			debug ("   ns_insert: Resetting tid of %s to %d \r\n", name, tid);
-			tmp->tid = tid;
-			break;
-		}
-		itr ++;
+	e = ns_lookup( ns, name );
+
+	if( (int) e == NOT_FOUND ) { 	// Add the entry after last entry.
+		debug ("\tns_insert: Adding %s, with tid: . \r\n", name, tid);
+		// Get the entry after the last one
+		e = &ns->entries[ns->nextEmpty++]; 
+		// Deep copy the name
+		strncpy (e->name, name, NAME_LEN);
+	} else {						// A match was found
+		debug ("\tns_insert: Overwriting tid of %s to %d \r\n", name, tid);
 	}
-	// Add the entry if the name isn't in the server.
-	if (itr >= ns->ptr) {
-		debug ("   ns_insert: Adding %s, since it is not in. \r\n", name);
-		ns->entries[ns->ptr++] = e; 
-	}
-	assert ( itr < NUM_NS_ENTRIES );
+
+	// Update the tid
+	e->tid = tid;
 }
 
-int ns_lookup (NameServer *ns, const char *name) {
-	debug ("ns_lookup: ns=%x name=%s \r\n", ns, name);
-	int itr = ns->ptr;
+NSEntry * ns_lookup (NameServer *ns, const char *name) {
+	debug ("ns_lookup: ns=%x name='%s' \r\n", ns, name);
+	int i;
 	
-	assert ( itr < NUM_NS_ENTRIES );
 	// Linearly traverse through the entries
-	while ( --itr >= 0 ) {
-		debug ("   %d->{%s, %d} == %s ? %d\r\n", itr, 
-			   ns->entries[itr].name, ns->entries[itr].tid,
-			   name, strcmp(name, ns->entries[itr].name));
+	for ( i = 0; i < ns->nextEmpty; i++ ) {
+		debug ("\t%d->{%s, %d} == %s ? %d\r\n", i, 
+			   ns->entries[i].name, ns->entries[i].tid,
+			   name, strcmp(name, ns->entries[i].name));
 		// If we've found a match,
-		if ( strcmp (ns->entries[itr].name, name) == 0 ) {
-			// Return the corresponding tid
-			return ns->entries[itr].tid;
+		if ( strcmp (ns->entries[i].name, name) == 0 ) {
+			// Return the matching NSEntry
+			return &ns->entries[i];
 		}
 	}
 	
 	// Otherwise, return an error
-	debug("	  ns_lookup: %s NOT_FOUND \r\n", name);
-	return NOT_FOUND;
+	debug("\tns_lookup: %s NOT_FOUND \r\n", name);
+	return (NSEntry *) NOT_FOUND;
 }
