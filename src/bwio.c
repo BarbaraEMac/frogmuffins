58a948717387add2a8489a2e18deb07133bde185
z4beth <z4beth@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-10-13 19:59:57 +0000

    Moved some functions from bwio to string

--- src/bwio.c
+++ src/bwio.c
@@ -9,32 +9,6 @@
 #include <bwio.h>
 
 /*
- * Some simple timer control
- *
- */
-int* clock_init( int clock_base, int enable, int val ) {
-    int *clock_ldr = (int *)( clock_base + LDR_OFFSET );
-    int *clock_ctl = (int *)( clock_base + CRTL_OFFSET );
-    // set the loader value, first disabling the timer
-    *clock_ctl = 0;
-    *clock_ldr = val;
-    // set the enable bit
-    if( enable ) { *clock_ctl = ENABLE_MASK; }
-    // return a pointer to the clock
-    return (int *)( clock_base + VAL_OFFSET );
-}
-
-void clock_stop( int clock_base ) {
-    clock_init( clock_base, 0, 0);
-}
-
-void wait( int ms ) {
-    int * clock = clock_init( TIMER1_BASE, 1, ms*2 );   // turn on the clock
-    while( *clock > 0 ) {}                              // wait for the clock to finish 
-    clock_stop( TIMER1_BASE );                          // turn off the clock
-}
-
-/*
  * The UARTs are initialized by RedBoot to the following state
  * 	115,200 bps
  * 	8 bits
@@ -118,7 +92,6 @@ int bwsendc( int channel, char c, int timeout ) {
     return 0;
 }
 
-
 int bwputc( int channel, char c ) {
 	int *flags, *data;
 	switch( channel ) {
@@ -137,11 +110,6 @@ int bwputc( int channel, char c ) {
 	while( ( *flags & TXFF_MASK ) ) ;
 	*data = c;
 	return 0;
-}
-
-char c2x( char ch ) {
-	if ( (ch <= 9) ) return '0' + ch;
-	return 'a' + ch - 10;
 }
 
 int bwputx( int channel, char c ) {
@@ -251,63 +219,10 @@ int bwreadc( int channel, char *c, int timeout) {
     return 0;
 }
 
-
-int bwa2d( char ch ) {
-	if( ch >= '0' && ch <= '9' ) return ch - '0';
-	if( ch >= 'a' && ch <= 'f' ) return ch - 'a' + 10;
-	if( ch >= 'A' && ch <= 'F' ) return ch - 'A' + 10;
-	return -1;
-}
-
-char bwa2i( char ch, char **src, int base, int *nump ) {
-	int num, digit, sign;
-	char *p;
-
-	p = *src; num = 0; sign = 1;
-    if( ch == '-' ) {
-        sign = -1;
-        ch = *p++;
-    }
-	while( ( digit = bwa2d( ch ) ) >= 0 ) {
-		if ( digit > base ) break;
-		num = num*base + digit;
-		ch = *p++;
-	}
-	*src = p; *nump = num * sign;
-	return ch;
-}
-
-void bwui2a( unsigned int num, unsigned int base, char *bf ) {
-	int n = 0;
-	int dgt;
-	unsigned int d = 1;
-	
-	while( (num / d) >= base ) d *= base;
-	while( d != 0 ) {
-		dgt = num / d;
-		num %= d;
-		d /= base;
-		if( n || dgt > 0 || d == 0 ) {
-			*bf++ = dgt + ( dgt < 10 ? '0' : 'a' - 10 );
-			++n;
-		}
-	}
-	*bf = 0;
-}
-
-void bwi2a( int num, char *bf ) {
-	if( num < 0 ) {
-		num = -num;
-		*bf++ = '-';
-	}
-	bwui2a( num, 10, bf );
-}
-
 void bwformat ( int channel, char *fmt, va_list va ) {
 	char bf[12];
 	char ch, lz;
 	int w;
-
 	
 	while ( ( ch = *(fmt++) ) ) {
 		if ( ch != '%' )
@@ -331,7 +246,7 @@ void bwformat ( int channel, char *fmt, va_list va ) {
 			case '7':
 			case '8':
 			case '9':
-				ch = bwa2i( ch, &fmt, 10, &w );
+				ch = atoi( ch, &fmt, 10, &w );
 				break;
 			}
 			switch( ch ) {
@@ -343,15 +258,15 @@ void bwformat ( int channel, char *fmt, va_list va ) {
 				bwputw( channel, w, 0, va_arg( va, char* ) );
 				break;
 			case 'u':
-				bwui2a( va_arg( va, unsigned int ), 10, bf );
+				uitoa( va_arg( va, unsigned int ), 10, bf );
 				bwputw( channel, w, lz, bf );
 				break;
 			case 'd':
-				bwi2a( va_arg( va, int ), bf );
+				itoa( va_arg( va, int ), bf );
 				bwputw( channel, w, lz, bf );
 				break;
 			case 'x':
-				bwui2a( va_arg( va, unsigned int ), 16, bf );
+				uitoa( va_arg( va, unsigned int ), 16, bf );
 				bwputw( channel, w, lz, bf );
 				break;
 			case '%':
@@ -368,85 +283,4 @@ void bwprintf( int channel, char *fmt, ... ) {
         va_start(va,fmt);
         bwformat( channel, fmt, va );
         va_end(va);
-}
-
-
-int scan ( char *src, char *fmt, va_list va ) {
-	char ch, ig, rd, *dest;
-	int w, *val;
-    unsigned int *uval;
-
-    rd = *(src++);	
-	while ( ( ch = *(fmt++) ) && rd ) {
-//        bwprintf( COM2, "\n\rch=%c, rd=%c", ch, rd);
-		if ( ch != '%' ) {
-            if( ch != rd )	return -1;
-            rd = *(src++);
-        } else {
-			ig =0; w = 0;
-			ch = *(fmt++);
-			if ( ch == '*' ) {
-				ig = 1; ch = *(fmt++);
-            }
-			switch ( ch ) {
-			case '1':
-			case '2':
-			case '3':
-			case '4':
-			case '5':
-			case '6':
-			case '7':
-			case '8':
-			case '9':
-				ch = bwa2i( ch, &fmt, 10, &w );
-				break;
-			}
-            //bwprintf( COM2, "\n\r2:ch=%c, rd=%c", ch, rd);
-			switch( ch ) {
-			case 'c':
-				*(va_arg( va, char* ) ) = rd;
-                rd = *(src++);
-				break;
-			case 's':
-                dest = va_arg( va, char* );
-                if( rd <= 32 || rd >= 127 ) return -5;
-                while( rd > 32 && rd < 127 ) { // stop at whitespace
-                    *dest++ = rd;
-                    rd = *src++;
-                }
-				break;
-			case 'u':
-                uval =	va_arg( va, unsigned int *);
-                rd = bwa2i(rd, &src, 10, uval );
-				break;
-			case 'd':
-				val= va_arg( va, int *);
-                rd = bwa2i(rd, &src, 10, val );
-				break;
-			case 'x':
-				val= va_arg( va, int *);
-                rd = bwa2i(rd, &src, 16, val );
-				break;
-			case '%':
-				if( rd != ch ) return -3;
-                rd = *(src++);
-				break;
-			}
-
-            //bwprintf( COM2, "\n\r3:ch=%c, rd=%c, val=%d, uval=%u", ch, rd, *val, uval);
-		}
-	}
-    if( !rd && ch ) return -2; // we ran out of input
-    return 0;
-}
-int sscanf( char *src, char *fmt, ... ) {
-        int res;
-        va_list va;
-        
-        va_start(va,fmt);
-        res = scan( src, fmt, va );
-        if( res != 0 ) return res;
-        va_end(va);
-
-        return 0;
 }
--- src/string.c
+++ src/string.c
@@ -4,6 +4,7 @@
  * dgoc
  */
 
+#include "bwio.h"
 #include "string.h"
 
 char * strncpy ( char *destination, const char * source, size_t num ) {
@@ -37,4 +38,145 @@ int strcmp (const char *str1, const char *str2) {
 	}
 	
 	return (*str1 < *str2) ? -1 : 1;
+}
+
+int scan ( char *src, char *fmt, va_list va ) {
+	char ch, ig, rd, *dest;
+	int w, *val;
+    unsigned int *uval;
+
+    rd = *(src++);	
+	while ( ( ch = *(fmt++) ) && rd ) {
+//        bwprintf( COM2, "\n\rch=%c, rd=%c", ch, rd);
+		if ( ch != '%' ) {
+            if( ch != rd )	return -1;
+            rd = *(src++);
+        } else {
+			ig =0; w = 0;
+			ch = *(fmt++);
+			if ( ch == '*' ) {
+				ig = 1; ch = *(fmt++);
+            }
+			switch ( ch ) {
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '8':
+			case '9':
+				ch = atoi( ch, &fmt, 10, &w );
+				break;
+			}
+            //bwprintf( COM2, "\n\r2:ch=%c, rd=%c", ch, rd);
+			switch( ch ) {
+			case 'c':
+				*(va_arg( va, char* ) ) = rd;
+                rd = *(src++);
+				break;
+			case 's':
+                dest = va_arg( va, char* );
+                if( rd <= 32 || rd >= 127 ) return -5;
+                while( rd > 32 && rd < 127 ) { // stop at whitespace
+                    *dest++ = rd;
+                    rd = *src++;
+                }
+				break;
+			case 'u':
+                uval =	va_arg( va, unsigned int *);
+                rd = atoi(rd, &src, 10, uval );
+				break;
+			case 'd':
+				val= va_arg( va, int *);
+                rd = atoi(rd, &src, 10, val );
+				break;
+			case 'x':
+				val= va_arg( va, int *);
+                rd = atoi(rd, &src, 16, val );
+				break;
+			case '%':
+				if( rd != ch ) return -3;
+                rd = *(src++);
+				break;
+			}
+
+            //bwprintf( COM2, "\n\r3:ch=%c, rd=%c, val=%d, uval=%u", ch, rd, *val, uval);
+		}
+	}
+    if( !rd && ch ) return -2; // we ran out of input
+    return 0;
+}
+
+int sscanf( char *src, char *fmt, ... ) {
+	int res;
+	va_list va;
+	
+	va_start(va,fmt);
+	res = scan( src, fmt, va );
+	if( res != 0 ) return res;
+	va_end(va);
+
+	return 0;
+}
+
+/** 
+ * The following functions have been borrowed from bwio and 
+ * renamed. We did not write them ourselves.
+ */
+int atod( char ch ) {
+	if( ch >= '0' && ch <= '9' ) return ch - '0';
+	if( ch >= 'a' && ch <= 'f' ) return ch - 'a' + 10;
+	if( ch >= 'A' && ch <= 'F' ) return ch - 'A' + 10;
+	return -1;
+}
+
+char atoi( char ch, char **src, int base, int *nump ) {
+	int num, digit, sign;
+	char *p;
+
+	p = *src; num = 0; sign = 1;
+    if( ch == '-' ) {
+        sign = -1;
+        ch = *p++;
+    }
+	while( ( digit = atod( ch ) ) >= 0 ) {
+		if ( digit > base ) break;
+		num = num*base + digit;
+		ch = *p++;
+	}
+	*src = p; *nump = num * sign;
+	return ch;
+}
+
+void uitoa( unsigned int num, unsigned int base, char *bf ) {
+	int n = 0;
+	int dgt;
+	unsigned int d = 1;
+	
+	while( (num / d) >= base ) d *= base;
+	while( d != 0 ) {
+		dgt = num / d;
+		num %= d;
+		d /= base;
+		if( n || dgt > 0 || d == 0 ) {
+			*bf++ = dgt + ( dgt < 10 ? '0' : 'a' - 10 );
+			++n;
+		}
+	}
+	*bf = 0;
+}
+
+void itoa( int num, char *bf ) {
+	if( num < 0 ) {
+		num = -num;
+		*bf++ = '-';
+	}
+	uitoa( num, 10, bf );
+}
+
+char ctox( char ch ) {
+	if ( (ch <= 9) ) return '0' + ch;
+	return 'a' + ch - 10;
 }
