ef44b2a271f7f0014c2794ffb0ab5b8164b2d19d
z4beth <z4beth@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-10-15 15:40:47 +0000

    A prelim clock server

--- trunk/server/clockserver.c
+++ trunk/server/clockserver.c
@@ -9,5 +9,50 @@
 
 #include "debug.h"
 #include "error.h"
-#include "nameserver.h"
+#include "clockserver.h"
+
+
+void cs_run () {
+	debug ("cs_run: The Clock Server is about to start. \r\n");	
+	ClockServer cs;
+	int 	   senderTid;
+	CSRequest  req;
+	int 	   ret, len;
+
+	// Initialize the Clock Server
+	cs_init (&cs);
+
+	FOREVER {
+		// Receive a server request
+		debug ("cs: is about to Receive. \r\n");
+		len = Receive ( &senderTid, (char *) &req, sizeof(CSRequest) );
+		debug ("cs: Received: fromTid=%d name='%s' type=%d  len=%d\r\n", senderTid, req.name, req.type, len);
+	
+		assert( len == sizeof(CSRequest) );
+
+		switch (req.type) {
+			case DELAY:
+				
+				break;
+			case TIME:
+				
+				break;
+			case DELAYUNTIL:
+				
+				break;
+			default:
+				ret = CS_INVALID_REQ_TYPE;
+				debug (1, "Clockserver request type is not valid.");
+				break;
+		}
+
+		debug ("cs: about to Reply to %d. \r\n", senderTid);
+		Reply ( senderTid, (char *) &ret, sizeof(int) );
+		debug ("cs: returned from Replying to %d. \r\n", senderTid);
+	}
+}
+
+int cs_init (ClockServer *cs) {
+
+}
 
