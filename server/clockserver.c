87847cfdcfbcba22f14a99d8e5fe2d9cf13103f5
z4beth <z4beth@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-10-15 15:33:05 +0000

    Adding clockserver files

---
+++ server/clockserver.c
@@ -0,0 +1,13 @@
+/**
+ * CS 452: Kernel
+ * becmacdo
+ * dgoc
+ */
+//#define DEBUG
+
+#include <bwio.h>
+
+#include "debug.h"
+#include "error.h"
+#include "nameserver.h"
+
---
+++ server/clockserver.h
@@ -0,0 +1,11 @@
+/**
+ * CS 451: kernel
+ * becmacdo
+ * dgoc
+ */
+
+#ifndef __CLOCK_SERVER_H__
+#define __CLOCK_SERVER_H__
+
+
+#endif
