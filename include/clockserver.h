4a6af1abe61557b99bba73799b6d79ed1be80e19
z4beth <z4beth@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-10-15 15:54:35 +0000

    Put the clock server name in the header

--- include/clockserver.h
+++ include/clockserver.h
@@ -7,5 +7,16 @@
 #ifndef __CLOCK_SERVER_H__
 #define __CLOCK_SERVER_H__
 
+#define CLOCK_NAME "ClockServer"
+
+typedef struct {
+
+
+} ClockServer;
+
+void cs_run ();
+
+int cs_init (ClockServer *cs);
+
 
 #endif
