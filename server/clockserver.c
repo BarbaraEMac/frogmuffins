f1c10106f14981e073ea868fd532554c8e3b4a38
z4beth <z4beth@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-10-15 19:58:49 +0000

    Removing clock.[h,c] and put the functionality into clockserver.[h.c]. Added a clock server request code for NOTIFY. Added the notifier run function

--- server/clockserver.c
+++ server/clockserver.c
@@ -40,6 +40,11 @@ void cs_run () {
 			case DELAYUNTIL:
 				
 				break;
+
+			case NOTIFY:
+
+
+				break;
 			default:
 				ret = CS_INVALID_REQ_TYPE;
 				debug (1, "Clockserver request type is not valid.");
@@ -54,5 +59,34 @@ void cs_run () {
 
 int cs_init (ClockServer *cs) {
 	RegisterAs (CLOCK_NAME);
+	
+	// Spawn a notifying helper task
+	Create( 1, &notifier_run );
 }
 
+
+void notifier_run() {
+
+}
+
+int* clock_init( int clock_base, int enable, int val ) {
+    int *clock_ldr = (int *)( clock_base + LDR_OFFSET );
+    int *clock_ctl = (int *)( clock_base + CRTL_OFFSET );
+    // set the loader value, first disabling the timer
+    *clock_ctl = 0;
+    *clock_ldr = val;
+    // set the enable bit
+    if( enable ) { *clock_ctl = ENABLE_MASK; }
+    // return a pointer to the clock
+    return (int *)( clock_base + VAL_OFFSET );
+}
+
+void clock_stop( int clock_base ) {
+    clock_init( clock_base, 0, 0);
+}
+
+void wait( int ms ) {
+    int * clock = clock_init( TIMER1_BASE, 1, ms*2 );   // turn on the clock
+    while( *clock > 0 ) {}                              // wait for the clock to finish 
+    clock_stop( TIMER1_BASE );                          // turn off the clock
+}
