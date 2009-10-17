9dcb29f6acfc858d0ca805ca5bfe40d8d13ec01d
z4beth <z4beth@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-10-17 17:04:11 +0000

    Adding drivers folder

---
+++ trunk/include/drivers.h
@@ -0,0 +1,14 @@
+/**
+ * CS 452: Kernel
+ * becmacdo
+ * dgoc
+ */
+
+#ifndef __DRIVERS_H__
+#define __DRIVERS_H__
+
+void timer1Driver (char *retBuf, int bufLen);
+
+void timer2Driver (char *retBuf, int bufLen);
+
+#endif
