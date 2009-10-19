f8527417e4f5bec39e71d71eb006f4ea750daaea
z4beth <z4beth@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-10-19 16:10:10 +0000

    forgot

--- include/shell.h
+++ include/shell.h
@@ -1,1 +1,20 @@
+/**
+ * CS 451: kernel
+ * becmacdo
+ * dgoc
+ */
 
+#ifndef __SHELL_H__
+#define __SHELL_H__
+
+/**
+ * Start the command prompt shell running.
+ */
+void shell_run ();
+
+/**
+ * Given an input command, error check it and execute it.
+ */
+void shell_exec ( char *command );
+
+#endif
