0853d5dc8098bc807ca22d594f7f1abc214cf2a2
z4beth <z4beth@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-10-23 20:36:51 +0000

    Adding a shell that runs train stuff

--- task/shell.c
+++ task/shell.c
@@ -1,7 +1,7 @@
  /*
  * dgoc_a1.c
  */
-//#define DEBUG
+#define DEBUG
 
 #include <bwio.h>
 #include <string.h>
@@ -11,8 +11,10 @@
 #include "debug.h"
 #include "nameserver.h"
 #include "requests.h"
+#include "serialio.h"
 #include "shell.h"
 #include "task.h"
+#include "traincontroller.h"
 
 #define FOREVER     for( ; ; )
 #define INPUT_LEN   60
@@ -21,43 +23,48 @@
 #define output(args...) bwputstr(COM2, "\033[36m"); bwprintf(COM2, args); bwputstr(COM2,"\033[37m")
 
 void shell_run ( ) {
-/*
- * TESTING WEIRD ASSEMBLER?? bug
- *
- * char o;
-	if( bwreadc( COM2, &o, 0 ) ) { o = 1;}
-
-//	asm(".WORD 0xDEADDEAD");
-	asm("#; stop looking here");
-	asm("mov r0, #1");
-	asm("mrs r1, SPSR");
-	asm("bl bwputr(PLT)");
- */
     debug ("shell_run\r\n");
-
-
 
 	// Initialize variables
     char *input, history[INPUT_HIST][INPUT_LEN];
-    int i = 0, h = 0;
+    int i = 0, h = 0, time;
+	TID nsTid;
+	TID csTid;
+	TID iosTid;
+	TID tcTid;
+	CSRequest csReq;
+
+	csReq.type  = TIME;
+	csReq.ticks = 0;
 
     // Set up the communication ports
 	bwsetfifo( COM2, OFF );
     
 	// Create the name server
 	debug ("Creating the name server. \r\n");
-	Create (2, &ns_run);
+	nsTid = Create (2, &ns_run);
 	
 	// Create the clock server
 	debug ("Creating the clock server. \r\n");
-	Create (2, &cs_run);
+	csTid = Create (2, &cs_run);
+
+	// Create the Serial I/O server
+	debug ("Creating the serial io server. \r\n");
+	iosTid = Create (2, &ios_run);
+
+	// Create the train controller
+	debug ("Creating the train controller. \r\n");
+	tcTid = Create (2, &tc_run);
 	
 	output ("Type 'h' for a list of commands.\r\n");
 
 	// Main loop
 	int k =0, l = 0;
     input = history[h++];
-	output ("\r > "); 
+	
+	time = Time(csTid);
+	output ("\r%02d:%02d:%02d> ", (((time*50)/1000)/60), (((time*50)/1000)%60),
+		    ((time*50)%1000)); 
     FOREVER {
 
         input[i] = 0;						// Clear the next character
@@ -68,13 +75,15 @@ void shell_run ( ) {
 				bwputstr ( COM2, "\n\r");
                 input[i+1] = 0;
                 
-				shell_exec( input );		// This may call Exit();
+				shell_exec( input, tcTid );	// This may call Exit();
                 
 				// Clear the input for next line
                 //input = history[h++];
                 //h++; h &= INPUT_HIST;
                 i = 0;
-				output ("\r > "); 
+				time = Time(csTid);
+				output ("\r%02d:%02d:%02d> ", (((time*50)/1000)/60),
+						(((time*50)/1000)%60), ((time*50)%1000));
             } else if( input[i] == '\b'
                     || input[i] == 127) {	// Backspace was pressed
 				if( i > 0 ) {
@@ -92,21 +101,23 @@ void shell_run ( ) {
             }
         }
         if( i == INPUT_LEN ) {
-            output( "Too many characters.\r\n" );
+            output( "\r\nToo many characters.\r\n" );
             i = 0;
 			
-			output ("\r > "); 
-        }
+			time = Time(csTid);
+			output ("\r%02d:%02d:%02d> ", (((time*50)/1000)/60),
+					(((time*50)/1000)%60), ((time*50)%1000));
+		}
     }
 }
 
 // Execute the command passed in
-void shell_exec( char *command ) {
-    int mask;
+void shell_exec( char *command, TID tcTid ) {
+	TCRequest tcReq;
+	int	  rpl;
 
     if ( sscanf(command, "q\r") >= 0 ) {
         Exit();
-        
     } 
 	else if( sscanf(command, "k1\r") >=0 ) {	// Run Kernel 1
 		Create (0, &k1_firstUserTask);
@@ -122,19 +133,46 @@ void shell_exec( char *command ) {
 		// K3 is asynchronous so we don't know when it will end
 		//output( "K3 is done executing.\r\n" );
 	}
-	else if( sscanf(command, "ua %x\r", &mask) >=0 ) {
-        output( "Changing UART1 to %04x.\n\r", mask );
-        int *flag = (int *)( UART1_BASE + UART_LCRH_OFFSET );
-        *flag = mask;
-        output( "UART1 SETTINGS: 0x%04x\n\r", *flag );
+	else if( sscanf(command, "rv %d\r", &tcReq.arg1) >=0 ) {
+    	tcReq.type = RV;
+		
+		Send(tcTid, (char*) &tcReq, sizeof(TCRequest), (char*)&rpl, sizeof(int));
+    } 
+	else if( sscanf(command, "st %d\r", &tcReq.arg1) >=0 ) {
+		tcReq.type = ST;
+		
+		Send(tcTid, (char*) &tcReq, sizeof(TCRequest), (char*)&rpl, sizeof(int));
+    }
+	else if( sscanf(command, "sw %d %c\r", &tcReq.arg1, (char*)&tcReq.arg2) >=0 ) {
+		tcReq.type = SW;
+		
+		Send(tcTid, (char*) &tcReq, sizeof(TCRequest), (char*)&rpl, sizeof(int));
+	}
+	else if( sscanf(command, "tr %d %d\r", &tcReq.arg1, &tcReq.arg2) >= 0 ) {
+    	tcReq.type = TR;
+
+		Send(tcTid, (char*) &tcReq, sizeof(TCRequest), (char*)&rpl, sizeof(int));
+    } 
+	else if( sscanf(command, "wh\r") >=0 ) {
+		tcReq.type = WH;
+		
+		Send(tcTid, (char*) &tcReq, sizeof(TCRequest), (char*)&rpl, sizeof(int));
     }
 	else if( sscanf(command, "h\r") >=0 ) {	// Get help!
-		output( "\t%s\r\n\t%s\r\n\t%s\r\n\t%s\r\n\t%s\r\n", 
+		output( "\t%s\r\n\t%s\r\n\t%s\r\n\t%s\r\n\t%s\r\n\t%s\r\n\t%s\r\n\t%s\r\n\t%s\r\n\t%s\r\n", 
 				"h = Help!", 
 				"k1 = Execute kernel 1 user tasks.", 
 				"k2 = Execute kernel 2 user tasks.", 
 				"k3 = Execute kernel 3 user tasks.", 
-				"ua = Change the UART." );
+				"++ Train Controller Commands ++",
+				"\t rv train_num  = Reverse specified train.",
+				"\t st switch_num = Display status of switch.",
+				"\t sw switch_num dir = Switch the switch in the sepcified direction.",
+				"\t tr train_num speed = Set the speed of the specified train.",
+				"\t wh = Display the last modified sensor.");
+	}
+	else {
+		output("\r\nUnknown command: %s\r\n", command);
 	}
 }
 
