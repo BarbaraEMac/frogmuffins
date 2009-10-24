35ee018bb6cda66be80ef0a39ae267094ec7e422
DanielGoc <DanielGoc@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-10-24 02:10:07 +0000

    added debug levels

--- include/debug.h
+++ include/debug.h
@@ -11,6 +11,13 @@
 #ifndef __DEBUG_H__
 #define __DEBUG_H__
 
+
+// Define debug statement macros
+#define assert(exp)
+#define debug(args...)
+#define error(code, msg) bwprintf (COM2, "\033[35mERROR: %d %s on line %d in file %s.\r\n\033[37m", code, msg, __LINE__, __FILE__)
+
+
 #ifdef DEBUG 
 
 void __assert (int test, const char *exp, int line, const char *file);
@@ -19,23 +26,20 @@ void __assert (int test, const char *exp, int line, const char *file);
  * Assert that the given expression is true.
  * If it isn't, the expression, line number, and file are output.
  */
+#undef assert
 #define assert(exp) __assert(exp, #exp, __LINE__, __FILE__ )
 
-/**
- * Print the string to standard output.
- */
+
+#if (DEBUG > 1)
+	/**
+	* Print the string to standard output.
+	*/
+#undef debug
 #define debug(args...) bwputstr( COM2, "\033[32m\t"); bwprintf( COM2, args ); bwputstr( COM2, "\033[37m")
-
-#else
-
-// GCC strips out do while's that do nothing
-#define assert(exp)
-
-// GCC strips out do while's that do nothing
-#define debug(args...) do {} while(0)
 
 #endif
 
-#define error(code, msg) bwprintf (COM2, "\033[35mERROR: %d %s on line %d in file %s.\r\n\033[37m", code, msg, __LINE__, __FILE__);
+#endif
+
 
 #endif
