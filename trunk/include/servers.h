8bc5bb58817f6b2c8119f9bc8e5cb12b5f870e2e
z4beth <z4beth@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-10-29 15:04:23 +0000

    Moved the private server stuff into the c files

--- trunk/include/servers.h
+++ trunk/include/servers.h
@@ -54,117 +54,25 @@ typedef struct {
 //--------------------------------Name Server----------------------------------
 //-----------------------------------------------------------------------------
 /**
- * A Name Server Entry
- * Stores a task name and task id.
- */
-typedef struct {
-	char name[NAME_LEN];	// The name of the task
-	TID  tid;				// The task id
-} NSEntry;
-
-/**
- * A Name Server
- * Stores a mapping between task id and their name.
- * User tasks can register with the NS to give a name.
- * User tasks can request a task id from the NS given a name.
- */
-typedef struct {
-	NSEntry entries[NUM_NS_ENTRIES]; // The entries for the Name Server
-	int 	nextEmpty;				 // A pointer to the next empty NS entry
-} NameServer;
-
-/**
- * Initialize the nameserver
- * ns - The name server that user tasks can register with.
- */
-void ns_init (NameServer *ns);
-
-/**
  * The main function for the Name Server task.
  */
 void ns_run ();
-
-/**
- * Insert a {name, tid} entry into the name server database.
- * ns - The name server that user tasks can register with.
- * name - The name for the tid
- * tid - The tid for the given name
- */
-void ns_insert (NameServer *ns, const char *name, int tid);
-
-/**
- * Returns the tid of the task that registered with the given name
- * ns - The name server that user tasks can register with.
- * name - The name for the tid
- * RETURN: The corresponding tid
- */
-NSEntry * ns_lookup (NameServer *ns, const char *name);
 
 //-----------------------------------------------------------------------------
 //-------------------------------Clock Server----------------------------------
 //-----------------------------------------------------------------------------
 
 /**
- * Any task that is sleeping on the clockserver will use this structure.
- */
-typedef struct sleeper Sleeper;
-struct sleeper {
-	TID tid;		// The id of the sleeping task
-	int endTime;	// The wake up time
-
-	Sleeper *next;	// Pointer to next sleeper in the queue
-	Sleeper *prev;	// Pointer to the previous sleeper in the queue
-};
-
-/**
  * All of the Clock Server work is done in here.
  */
 void cs_run ();
 
-/**
- * Initialize the clock server.
- */
-int cs_init (Sleeper **sleepersQ);
-
-// List operators
-void list_insert ( Sleeper **head, Sleeper *toAdd );
-
-void list_remove ( Sleeper **head, Sleeper *toAdd );
-
-// Notifier
-/**
- * All of the Clock Server's notifier's work is done in here.
- */
-void notifier_run();
-
-/**
- * Initialize the Clock Server's Notifier.
- */
-void notifier_init();
-
 //-----------------------------------------------------------------------------
 //--------------------------------Serial IO------------------------------------
 //-----------------------------------------------------------------------------
-typedef struct {
-	char sendBuffer[NUM_ENTRIES][ENTRY_LEN];
-	char recvBuffer[NUM_ENTRIES];
-	
-	int sendPtr;
-
-	int sEmpPtr;
-	int sFulPtr;
-	int rEmpPtr;
-	int rFulPtr;
-} SerialIOServer;
-
 /**
  * All of the Serial IO work is done in here.
  */
 void ios_run ();
-
-/**
- * Initialize the Serial IO Server structure.
- */
-void ios_init (SerialIOServer *server);
 
 #endif
--- trunk/server/nameserver.c
+++ trunk/server/nameserver.c
@@ -11,6 +11,50 @@
 #include "error.h"
 #include "servers.h"
 
+//-----------------------------------------------------------------------------
+/**
+ * A Name Server Entry
+ * Stores a task name and task id.
+ */
+typedef struct {
+	char name[NAME_LEN];	// The name of the task
+	TID  tid;				// The task id
+} NSEntry;
+
+/**
+ * A Name Server
+ * Stores a mapping between task id and their name.
+ * User tasks can register with the NS to give a name.
+ * User tasks can request a task id from the NS given a name.
+ */
+typedef struct {
+	NSEntry entries[NUM_NS_ENTRIES]; // The entries for the Name Server
+	int 	nextEmpty;				 // A pointer to the next empty NS entry
+} NameServer;
+
+/**
+ * Initialize the nameserver
+ * ns - The name server that user tasks can register with.
+ */
+void ns_init (NameServer *ns);
+
+/**
+ * Insert a {name, tid} entry into the name server database.
+ * ns - The name server that user tasks can register with.
+ * name - The name for the tid
+ * tid - The tid for the given name
+ */
+void ns_insert (NameServer *ns, const char *name, int tid);
+
+/**
+ * Returns the tid of the task that registered with the given name
+ * ns - The name server that user tasks can register with.
+ * name - The name for the tid
+ * RETURN: The corresponding tid
+ */
+NSEntry * ns_lookup (NameServer *ns, const char *name);
+//-----------------------------------------------------------------------------
+
 void ns_run () {
 	debug ("ns_run: The Name Server is about to start. \r\n");	
 	NameServer	ns;
