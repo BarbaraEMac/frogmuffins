e190309f7fd75b43f04b0a936533635fd8e450eb
DanielGoc <DanielGoc@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-11-08 22:57:22 +0000

    assert failuer now goes gack to redboot, unfortunately we can't rerun w/o craching

--- trunk/src/debug.c
+++ trunk/src/debug.c
@@ -13,6 +13,9 @@ void __assert ( int test, char *exp, int line, char *file ) {
 	
 	if ( !test ) {
 		bwprintf (COM2, "\033[31mAssert Failure: %s at line #%d in file %s.\033[37m\n\r", exp, line, file );
-		asm("mov pc, #0");
+		writeMemory(0x28, 0x174c8 );		// swi goes back to redboot
+
+		//asm("mov pc, #0");
+
 	}
 }
