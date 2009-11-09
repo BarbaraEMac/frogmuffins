1987b7b94e257463b7ae2cd8a9a0e3c3e582cc40
DanielGoc <DanielGoc@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-11-09 19:22:05 +0000

    fixed parse_model

--- trunk/train/model.c
+++ trunk/train/model.c
@@ -232,7 +232,7 @@ int parse_model_str(char* str, TrackModel* model)
   return 0;
 }
 
-int parse_model(int trackId, track_model_t* model)
+int parse_model(int trackId, TrackModel* model)
 {
 char * track[] = {
 "track 72\
@@ -690,22 +690,14 @@ char * track[] = {
 68 DE9 stop 8 10 285 1\
   12 ahead 70\
 69 DE10 stop 9 10 265 1\
-  13 ahead 70"};
+  13 ahead 70"
+};
 
 
-	/*
-  FILE* file;
-  file = fopen(filename, "r");
-  if (file == 0) {
-    eprintf("Could not open %s.\n", filename);
-    return -1;
-  }
-  */
 	if( trackId < 0 || trackId > 1 ) {
 		eprintf("Invalid track id %d.\r\n", trackId);
 	}
-	model->nodes = 0;
-	return parse_model_file(track[track_id], model);
+	return parse_model_str(track[trackId], model);
   
 }
 
