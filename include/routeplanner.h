283aef89eb97b7923a7ad0e3fdfb33efdee68280
z4beth <z4beth@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-11-10 23:31:55 +0000

    Adding a completed and debugged Floyd Warshall

--- include/routeplanner.h
+++ include/routeplanner.h
@@ -7,9 +7,14 @@
 #ifndef __ROUTE_PLANNER_H__
 #define __ROUTE_PLANNER_H__
 
+#include "model.h"
 
-#include "requests.h"
-
+typedef struct {
+	int   speed;
+	int   time;
+	Node *currLoc;
+	Node *dest;
+} RPRequest;
 
 void routeplanner_run();
 
--- train/routeplanner.c
+++ train/routeplanner.c
@@ -3,15 +3,15 @@
  * becmacdo
  * dgoc
  */
-#define DEBUG 1
+#define DEBUG 2
 
 #include <string.h>
 #include <ts7200.h>
 
 #include "debug.h"
 #include "error.h"
-#include "model.h"
 #include "requests.h"
+#include "routeplanner.h"
 #include "servers.h"
 #include "train.h"
 
@@ -25,87 +25,156 @@ typedef struct {
 	// what will this look like? How do we implement it?
 } Reservation;
 
+typedef struct {
+	int 		dists[MAX_NUM_NODES][MAX_NUM_NODES];
+	int			paths[MAX_NUM_NODES][MAX_NUM_NODES];
 
+
+	
+} RoutePlanner;
+
+
+void routeplanner_init();
+
+// Shortest Path Algorithms
+void fwarsh ( TrackModel *model, RoutePlanner *rp, int n );
+int cost ( TrackModel *model, int idx1, int idx2 );
+void outputPath (TrackModel *model, RoutePlanner *rp, int i, int j );
 // ----------------------------------------------------------------------------
 
-void routeplaner_run() {
-	
-	TrackModel *model;
+void routeplanner_run() {
+	debug ("routeplanner_run\r\n");
+	TrackModel  	model;
+	RoutePlanner	rp;
+	int 			senderTid;
+	RPRequest		msg;
 
-	// Get the model from the first task
+	// HACK
+	parse_model( TRACK_B, &model );
+
+	// TODO: Get the model from the first task
 	// Send ();
 
 	// Initialize shortest paths using model
+	fwarsh (&model, &rp, model.num_nodes); 
+
 	// Initialize track reservation system (nothing is reserved)
 	
-	// FOREVER 
-	// Receive from client - current location, destination, speed?, current time?
+	FOREVER {
+		Receive ( &senderTid, (char*)&msg, sizeof(RPRequest) );
+		// Receive from client - current location, destination, speed?, current time?
 	// Determine shortest path (observing blockages / reservations)
 	// Return the speed and direction? Next landmark? (does train even know the track topology?)
 
 
+
+	}
+
+	Exit(); // This will never be called
+
 }
 
-void routeplaner_init() {
+void routeplanner_init() {
+	
+
+
 
 }
 
-
+// O(n^3) + cost lookup time
+//
 // Floyd-Warshall algorithm from http://www.joshuarobinson.net/docs/fwarsh.html.
 // 
 //  Solves the all-pairs shortest path problem using Floyd-Warshall algorithm
-//  Input:  n, number of nodes
-//          connectivity matrix cost, where 0 means disconnected
-//             and distances are all positive. 
-//             Array of ints of length (n*n).
-//  Output: retDist - shortest path distances(the answer)
+//  Input:  n - number of nodes
+//          model - To get the cost information.
+//  Output: retDist - shortest path dists(the answer)
 //          retPred - predicate matrix, useful in reconstructing shortest routes
-void fwarsh ( int n, int *cost, int **retDist, int **retPred ) {
+void fwarsh ( TrackModel *model, RoutePlanner *rp, int n ) {
+	int  i, j, k; // Loop counters
 	
-	int *dist = *retDist;		// Copy over to make pointer stuff easier
-	int *pred = *retPred;
-	int  i,j,k; 				// Loop counters
-
-	// Initialize data structures
-	memoryset(dist, 0, sizeof(int)*n*n);
-	memoryset(pred, 0, sizeof(int)*n*n);
-
 	// Algorithm initialization
-	for (i=0; i < n; i++) {
-		for (j=0; j < n; j++) {
-			if (cost[i*n+j] != 0)
-				dist[i*n+j] = cost[i*n + j];
-			else
-				dist[i*n+j] = INT_MAX; //disconnected
-
-			if (i==j)  //diagonal case
-				dist[i*n+j] = 0;
-
-			if ((dist[i*n + j] > 0) && (dist[i*n+j] < INT_MAX))
-				pred[i*n+j] = i;
+	for ( i = 0; i < n; i++ ) {
+		for ( j = 0; j < n; j++ ) {
+			
+			// INT_MAX if no link; 0 if i == j
+			rp->dists[i][j] = cost(model, i, j); 
+			
+			// Init to garbage
+			rp->paths[i][j] = -1;
 		}
 	}
- 
+
   	// Main loop of the algorithm
-	for (k=0; k < n; k++) {
-		for (i=0; i < n; i++) {
-			for (j=0; j < n; j++) {
-				if (dist[i*n+j] > (dist[i*n+k] + dist[k*n+j])) {
-	  				dist[i*n+j] = dist[i*n+k] + dist[k*n+j];
-	  				pred[i*n+j] = k; 
-	  				//printf("updated entry %d,%d with %d\n", i,j, dist[i*n+j]);
+	for ( k = 0; k < n; k++ ) {
+		for ( i = 0; i < n; i++ ) {
+			for ( j = 0; j < n; j++ ) {
+				
+				if (rp->dists[i][j] > (rp->dists[i][k] + rp->dists[k][j])) {
+	  				
+					rp->dists[i][j] = rp->dists[i][k] + rp->dists[k][j];
+	  				rp->paths[i][j] = k; 
 				}
       		}
     	}
   	}
 
-  	/* 
-	// Print out the results table of shortest distances
-  	for (i=0; i < n; i++) {
-    	for (j=0; j < n; j++)
-      		printf("%g \n", dist[i*n+j]);
+	/*
+	// Print out the results table of shortest rp->distss
+  	for ( i = 0; i < 1; i++ ) {
+    	for ( j = 0; j < n; j++ ) {
+      		
+			if ( rp->dists[i][j] != INT_MAX ) {
+		//		debug("DIST: (%s,%s)=%d THROUGH ", model->nodes[i].name, 
+		//									   model->nodes[j].name, 
+			//								   rp->dists[i][j]);
+			outputPath(model, rp, i ,j);		
+			printf ("%s\r\n", model->nodes[j].name);
+			}
+		}
     }
 	*/
-
 }  //end of fwarsh()
 
+int cost (TrackModel *model, int idx1, int idx2) {
+	//debug ("cost: i=%d j=%d model=%x\r\n", idx1, idx2, model);
+	int itr = 0;
+	int i, j;
+
+	if ( idx1 == idx2 ) {
+		return 0;
+	}
+
+	// Find the lower # of edges to reduce search time
+	if ( model->nodes[idx1].type < model->nodes[idx2].type ) {
+		i = idx1;
+		j = idx2;
+	} else {
+		i = idx2;
+		j = idx1;
+	}
+	
+	// Check each edge in O(3) time
+	//debug ("cost: itr=%d type=%d\r\n", itr, model->nodes[i].type);
+	while ( itr < (int) model->nodes[i].type ) {
+		// If the edge reaches the destination,
+		if ( model->nodes[i].edges[itr].dest == j ) {
+			// Return the distance between them
+			return model->nodes[i].edges[itr].distance; 
+		}
+
+		// Advance the iterator
+		itr += 1;
+	}
+	return INT_MAX;
+}
+
+void outputPath (TrackModel *model, RoutePlanner *rp, int i, int j) {
+	if ( rp->paths[i][j] == -1 ) {
+		printf ("%s> ", model->nodes[i].name);
+	}
+	else {
+		outputPath (model, rp, i, rp->paths[i][j]);
+		outputPath (model, rp, rp->paths[i][j], j);
+	}
+}
