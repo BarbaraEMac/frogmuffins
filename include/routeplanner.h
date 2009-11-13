b433bfc0e1b5b3e37422f4b3ef363a357bbd7042
z4beth <z4beth@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-11-13 21:00:27 +0000

    Added more functionality to the train. Also added things to the RPReply so that the train knows that to do

--- include/routeplanner.h
+++ include/routeplanner.h
@@ -23,14 +23,15 @@ typedef enum RPType {
 } RPType;
 
 typedef struct {
-	RPType	 type;		// Route Planner request type
+	RPType	type;		// Route Planner request type
 	
-	int		 trainId;	// Unique id for train
-	int   	 avgSpeed;	// Average speed of a train
+	int		trainId;	// Unique id for train
+	int   	avgSpeed;	// Average speed of a train
 						// between the past sensors
-
 	int		idx1; 		// Current Sensor, Start Location
-	int 	idx2; 		// Previous Sensor, Destination
+	int 	idx2; 		// Previous Sensor
+
+	int		dest;		// Destination Location Idx
 } RPRequest;
 
 typedef struct {
@@ -43,9 +44,16 @@ typedef struct {
 		int totalDist;	// stopping distance
 		int minDist;	// Minimum distance b/w nodes
 	};
-	int checkinDist;	// Distance to next switch
 	
-	Path	path;
+	int checkinDist;	// Distance to next checkin location
+
+	Path path;			// Path of indices from s -> t
+	Path prediction;	// Prediction for the next sensors
+
+	int switchId;		// Id of the next switch
+	SwitchDir switchDir;// 'S' OR 'C' if the switch needs to be thrown
+						// 0 		  Otherwise
+
 } RPReply;
 
 void rp_run();
