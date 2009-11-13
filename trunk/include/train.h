bb5c1add693e5f49d088d7a296670a98d74b4256
z4beth <z4beth@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-11-13 23:39:28 +0000

    Prelim train stuff

--- trunk/include/train.h
+++ trunk/include/train.h
@@ -10,6 +10,12 @@
 
 #include "requests.h"
 
+typedef struct {
+	int  id;			// Identifying number
+	int  currLoc;		// Current Location
+	int	 prevLoc;		// Previous Location
+	int  dest;			// Desired End Location
+} TrainInit;
 
 void train_run();
 
