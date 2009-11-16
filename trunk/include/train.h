fb5bbbaa9728ca382dafea402ac50fe3c2b2d8db
DanielGoc <DanielGoc@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-11-16 21:34:49 +0000

    added statistics for mean speed finding, using getstray to figure out where the train is if lost, preparing code for stopping distances

--- trunk/include/train.h
+++ trunk/include/train.h
@@ -10,10 +10,16 @@
 
 #include "requests.h"
 
+typedef enum _train_mode {
+	NORMAL = 1,
+	CALIBRATION = 2
+} TrainMode;
+
 typedef struct {
 	int  id;			// Identifying number
 	int  currLoc;		// Current Location
 	int  dest;			// Desired End Location
+	TrainMode mode;	// whether we're running calibration
 } TrainInit;
 
 void train_run();
