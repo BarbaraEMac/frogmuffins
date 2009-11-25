d9183e85b2a73aac399055ea71f627e0d3d56979
z4beth <z4beth@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-11-25 01:47:27 +0000

    Working a little bit better

--- trunk/include/train.h
+++ trunk/include/train.h
@@ -11,15 +11,10 @@
 #include "requests.h"
 
 typedef enum _train_mode {
-//	INIT_LOC = 1, 	// First go to a particular place
-//	INIT_DIR,		// then turn around to go around the loop in proper dir
-//	CAL_SPEED,		// Run around a bunch of times until speed is nice
-//	CAL_STOP,		// Calibrate stopping distance
-//	CAL_WAIT,		// Waiting for the train to stop
 	CAL_SPEED = 1,	// Calibrate the running speed
 	CAL_STOP,		// Calibrate the stopping distance
 	IDLE,			// Ready for input
-	NORMAL			// Going from place A to place B
+	DRIVE			// Going from place A to place B
 } TrainMode;
 
 typedef enum _train_request {
