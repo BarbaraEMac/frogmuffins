6db9de4dce490dfc5172c9a685829179324bb754
DanielGoc <DanielGoc@cc31e340-ac7e-11de-8ba5-91705c1537ed>
2009-12-04 20:31:55 +0000

    added track A x,y data

--- train/model.c
+++ train/model.c
@@ -234,239 +234,239 @@ int parse_model(int trackId, TrackModel* model)
 // A
 char * track[] = {
 " track 72 \
-0 A1 sensor 0 100 10 2 \
+0 A1 sensor 0 733 67 2 \
 	51 ahead 218 \
-	66 behind 500 \
-1 A3 sensor 1 135 85 2 \
+	66 behind 520 \
+1 A3 sensor 1 740 350 2 \
 	53 ahead 62 \
 	15 behind 440 \
-2 A5 sensor 2 160 285 2 \
+2 A5 sensor 2 915 1170 2 \
 	42 ahead 216 \
-	12 behind 649 \
-3 A7 sensor 3 100 265 2 \
+	12 behind 665 \
+3 A7 sensor 3 753 1093 2 \
 	13 ahead 473 \
 	41 behind 211 \
-4 A9 sensor 4 70 245 2 \
+4 A9 sensor 4 565 1020 2 \
 	11 ahead 289 \
 	40 behind 210 \
-5 A11 sensor 5 40 225 2 \
-	40 ahead 410 \
-	69 behind 60 \
-6 A13 sensor 6 70 30 2 \
+5 A11 sensor 5 305 940 2 \
+	40 ahead 500 \
+	71 behind 50 \
+6 A13 sensor 6 565 147 2 \
 	43 ahead 215 \
 	65 behind 320 \
-7 A15 sensor 7 40 50 2 \
-	67 ahead 145 \
-	43 behind 394 \
-8 B1 sensor 8 290 235 2 \
+7 A15 sensor 7 400 215 2 \
+	70 ahead 150 \
+	43 behind 400 \
+8 B1 sensor 8 1380 1020 2 \
 	30 ahead 398 \
 	55 behind 225 \
-9 B3 sensor 9 290 215 2 \
-	16 ahead 230 \
+9 B3 sensor 9 1380 953 2 \
+	16 ahead 205 \
 	55 behind 220 \
-10 B5 sensor 10 290 35 2 \
+10 B5 sensor 10 1380 147 2 \
 	25 ahead 405 \
 	52 behind 214 \
-11 B7 sensor 11 20 245 2 \
+11 B7 sensor 11 290 1020 2 \
 	4 ahead 289 \
-	68 behind 60 \
-12 B9 sensor 12 20 285 2 \
-	2 ahead 649 \
-	70 behind 70 \
-13 B11 sensor 13 20 265 2 \
+	67 behind 50 \
+12 B9 sensor 12 250 1170 2 \
+	2 ahead 665 \
+	68 behind 55 \
+13 B11 sensor 13 270 1093 2 \
 	3 ahead 473 \
-	71 behind 70 \
-14 B13 sensor 14 365 180 2 \
+	69 behind 55 \
+14 B13 sensor 14 1660 810 2 \
 	58 ahead 224 \
-	31 behind 202 \
-15 B15 sensor 15 135 185 2 \
+	31 behind 205 \
+15 B15 sensor 15 740 786 2 \
 	1 ahead 440 \
 	54 behind 70 \
-16 C1 sensor 16 325 180 2 \
-	9 ahead 230 \
-	61 behind 228 \
-17 C3 sensor 17 420 285 2 \
+16 C1 sensor 16 1530 810 2 \
+	9 ahead 205 \
+	61 behind 208 \
+17 C3 sensor 17 1895 1170 2 \
 	64 ahead 410 \
 	44 behind 226 \
-18 C5 sensor 18 220 260 2 \
+18 C5 sensor 18 1090 1093 2 \
 	45 ahead 79 \
 	54 behind 411 \
-19 C7 sensor 19 265 285 2 \
+19 C7 sensor 19 1275 1170 2 \
 	57 ahead 215 \
 	42 behind 145 \
-20 C9 sensor 20 200 235 2 \
+20 C9 sensor 20 1020 1020 2 \
 	54 ahead 305 \
 	55 behind 146 \
-21 C11 sensor 21 200 35 2 \
+21 C11 sensor 21 1035 147 2 \
 	52 ahead 140 \
 	53 behind 314 \
-22 C13 sensor 22 245 11 2 \
-	35 ahead 877 \
+22 C13 sensor 22 1210 67 2 \
+	35 ahead 875 \
 	50 behind 60 \
-23 C15 sensor 23 294 260 2 \
+23 C15 sensor 23 1380 1093 2 \
 	29 ahead 405 \
 	45 behind 223 \
-24 D1 sensor 24 365 90 2 \
-	59 ahead 227 \
-	33 behind 200 \
-25 D3 sensor 25 400 35 2 \
+24 D1 sensor 24 1660 350 2 \
+	59 ahead 207 \
+	33 behind 205 \
+25 D3 sensor 25 1785 147 2 \
 	49 ahead 223 \
 	10 behind 405 \
-26 D5 sensor 26 550 70 2 \
+26 D5 sensor 26 2405 290 2 \
 	34 ahead 375 \
 	48 behind 213 \
-27 D7 sensor 27 550 42 2 \
+27 D7 sensor 27 2405 210 2 \
 	48 ahead 290 \
 	35 behind 375 \
-28 D9 sensor 28 550 225 2 \
-	37 ahead 361 \
+28 D9 sensor 28 2405 962 2 \
+	37 ahead 375 \
 	47 behind 291 \
-29 D11 sensor 29 400 260 2 \
+29 D11 sensor 29 1785 1093 2 \
 	23 ahead 405 \
 	46 behind 220 \
-30 D13 sensor 30 400 235 2 \
+30 D13 sensor 30 1785 1020 2 \
 	8 ahead 398 \
 	56 behind 225 \
-31 D15 sensor 31 400 215 2 \
-	14 ahead 202 \
+31 D15 sensor 31 1785 953 2 \
+	14 ahead 205 \
 	56 behind 230 \
-32 E1 sensor 32 325 90 2 \
+32 E1 sensor 32 1530 350 2 \
 	60 ahead 222 \
-	39 behind 203 \
-33 E3 sensor 33 400 50 2 \
-	24 ahead 200 \
+	39 behind 205 \
+33 E3 sensor 33 1785 210 2 \
+	24 ahead 205 \
 	49 behind 220 \
-34 E5 sensor 34 470 35 2 \
+34 E5 sensor 34 2075 147 2 \
 	26 ahead 375 \
 	49 behind 74 \
-35 E7 sensor 35 470 11 2 \
+35 E7 sensor 35 2075 67 2 \
 	27 ahead 375 \
-	22 behind 877 \
-36 E9 sensor 36 550 200 2 \
+	22 behind 875 \
+36 E9 sensor 36 2405 885 2 \
 	47 ahead 215 \
 	38 behind 375 \
-37 E11 sensor 37 470 260 2 \
-	28 ahead 361 \
+37 E11 sensor 37 2075 1093 2 \
+	28 ahead 375 \
 	46 behind 65 \
-38 E13 sensor 38 470 235 2 \
+38 E13 sensor 38 2075 1020 2 \
 	56 ahead 62 \
 	36 behind 375 \
-39 E15 sensor 39 290 50 2 \
+39 E15 sensor 39 1380 210 2 \
 	52 ahead 230 \
-	32 behind 203 \
-40 SW1 switch 1 140 255 curved 3 \
-	5 straight 410 \
+	32 behind 205 \
+40 SW1 switch 1 775 1045 curved 3 \
+	5 straight 500 \
 	4 curved 210 \
 	41 behind 191 \
-41 SW2 switch 2 180 275 curved 3 \
+41 SW2 switch 2 950 1125 curved 3 \
 	40 straight 191 \
 	3 curved 211 \
 	42 behind 182 \
-42 SW3 switch 3 230 285 curved 3 \
+42 SW3 switch 3 1130 1170 curved 3 \
 	2 straight 216 \
 	41 curved 182 \
 	19 behind 145 \
-43 SW4 switch 4 140 20 straight 3 \
-	7 straight 394 \
+43 SW4 switch 4 775 110 straight 3 \
+	7 straight 400 \
 	6 curved 215 \
 	51 behind 185 \
-44 SW5 switch 5 375 285 curved 3 \
+44 SW5 switch 5 1680 1170 curved 3 \
 	17 straight 226 \
 	46 curved 334 \
 	57 behind 187 \
-45 SW6 switch 6 245 260 straight 3 \
+45 SW6 switch 6 1170 1093 straight 3 \
 	23 straight 223 \
 	57 curved 338 \
 	18 behind 79 \
-46 SW7 switch 7 447 260 straight 3 \
+46 SW7 switch 7 2010 1093 straight 3 \
 	29 straight 220 \
 	44 curved 334 \
 	37 behind 65 \
-47 SW8 switch 8 570 166 curved 3 \
+47 SW8 switch 8 2480 680 curved 3 \
 	28 straight 291 \
 	36 curved 215 \
 	48 behind 193 \
-48 SW9 switch 9 570 111 curved 3 \
+48 SW9 switch 9 2480 495 curved 3 \
 	27 straight 290 \
 	26 curved 213 \
 	47 behind 193 \
-49 SW10 switch 10 445 35 straight 3 \
+49 SW10 switch 10 2010 147 straight 3 \
 	25 straight 223 \
 	33 curved 220 \
 	34 behind 74 \
-50 SW11 switch 11 220 11 curved 3 \
+50 SW11 switch 11 1140 67 curved 3 \
 	51 straight 190 \
 	53 curved 445 \
 	22 behind 60 \
-51 SW12 switch 12 180 11 curved 3 \
+51 SW12 switch 12 950 67 curved 3 \
 	0 straight 218 \
 	43 curved 185 \
 	50 behind 190 \
-52 SW13 switch 13 245 35 straight 3 \
+52 SW13 switch 13 1170 147 straight 3 \
 	10 straight 214 \
 	39 curved 230 \
 	21 behind 140 \
-53 SW14 switch 14 150 70 curved 3 \
+53 SW14 switch 14 775 305 curved 3 \
 	50 straight 445 \
 	21 curved 314 \
 	1 behind 62 \
-54 SW15 switch 15 150 200 curved 3 \
+54 SW15 switch 15 775 845 curved 3 \
 	18 straight 411 \
 	20 curved 305 \
 	15 behind 70 \
-55 SW16 switch 16 245 235 straight 3 \
+55 SW16 switch 16 1170 1020 straight 3 \
 	8 straight 225 \
 	9 curved 220 \
 	20 behind 146 \
-56 SW17 switch 17 445 235 straight 3 \
+56 SW17 switch 17 2010 1020 straight 3 \
 	30 straight 225 \
 	31 curved 230 \
 	38 behind 62 \
-57 SW18 switch 18 315 285 curved 3 \
+57 SW18 switch 18 1490 1170 curved 3 \
 	19 straight 215 \
 	45 curved 338 \
 	44 behind 187 \
-61 SW99 switch 0x99 345 160 curved 3 \
+61 SW99 switch 0x99 1595 620 curved 3 \
 	62 straight 230 \
-	16 curved 228 \
+	16 curved 208 \
 	58 behind 24 \
-58 SW9A switch 0x9a 345 150 straight 3 \
+58 SW9A switch 0x9a 1595 600 straight 3 \
 	61 straight 24 \
 	14 curved 224 \
 	60 behind 32 \
-59 SW9B switch 0x9b 345 110 straight 3 \
+59 SW9B switch 0x9b 1595 540 straight 3 \
 	63 straight 247 \
-	24 curved 227 \
+	24 curved 207 \
 	60 behind 24 \
-60 SW9C switch 0x9c 345 120 curved 3 \
+60 SW9C switch 0x9c 1595 560 curved 3 \
 	59 straight 24 \
 	32 curved 222 \
 	58 behind 32 \
-62 DE1 stop 0 345 200 1 \
+62 DE1 stop 0 1595 850 1 \
 	61 ahead 230 \
-63 DE2 stop 1 345 70 1 \
+63 DE2 stop 1 1595 290 1 \
 	59 ahead 247 \
-64 DE3 stop 2 500 285 1 \
+64 DE3 stop 2 2310 1170 1 \
 	17 ahead 410 \
-65 DE4 stop 3 10 30 1 \
+65 DE4 stop 3 230 147 1 \
 	6 ahead 320 \
-66 DE5 stop 4 10 10 1 \
-	0 ahead 500 \
-67 DE6 stop 5 20 50 1 \
-	7 ahead 145 \
-68 DE7 stop 6 10 245 1 \
-	11 ahead 60 \
-69 DE8 stop 7 20 225 1 \
-	5 ahead 60 \
-70 DE9 stop 8 10 285 1 \
-	12 ahead 70 \
-71 DE10 stop 9 10 265 1 \
-	13 ahead 70",
+66 DE5 stop 4 210 67 1 \
+	0 ahead 520 \
+67 DE7 stop 6 240 1020 1 \
+	11 ahead 50 \
+68 DE9 stop 8 195 1170 1 \
+	12 ahead 55 \
+69 DE10 stop 9 215 1093 1 \
+	13 ahead 55 \
+70 DE6 stop 5 250 215 1 \
+	7 ahead 150 \
+71 DE8 stop 7 255 940 1 \
+	5 ahead 50",
 // B
 " track 70 \
 0 A1 sensor 0 733 67 2 \
 	51 ahead 218 \
-	66 behind 500 \
+	66 behind 520 \
 1 A3 sensor 1 740 350 2 \
 	53 ahead 62 \
 	15 behind 440 \
@@ -681,7 +681,7 @@ char * track[] = {
 65 DE4 stop 3 230 147 1 \
 	6 ahead 320 \
 66 DE5 stop 4 210 67 1 \
-	0 ahead 500 \
+	0 ahead 520 \
 67 DE7 stop 6 290 1020 1 \
 	11 ahead 60 \
 68 DE9 stop 8 250 1170 1 \
